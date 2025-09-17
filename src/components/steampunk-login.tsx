eb5502f94426ef194314b9b7e0e69fbbeaf968ab
Akash Rathaur <akash.s.rathaur@gmail.com>
2025-09-17 09:16:41 +0000

    Try fixing this error: `Console Error: Invalid DOM property `transform-o

--- src/components/steampunk-login.tsx
+++ src/components/steampunk-login.tsx
@@ -54,19 +54,19 @@ export function SteampunkLogin() {
         </g>
 
         {/* Gear system */}
-        <g id="gear-system" transform-origin="center" className={isSubmitting ? 'animate-spin-slow' : ''}>
+        <g id="gear-system" style={{transformOrigin: "center"}} className={isSubmitting ? 'animate-spin-slow' : ''}>
            {/* Big Gear */}
            <path d="M 330 80 a 40 40 0 1 0 0 1  M 290 80 L 288 75 L 292 70 L 295 70 L 297 75 L 293 80 Z M 298.66 82.5 L 294.66 77.5 L 300.31 73.83 L 302.99 75.17 L 301.34 80.5 Z M 305.88 88.66 L 300.22 84.32 L 305.19 79.88 L 308.53 80.5 L 308.84 86.66 Z M 313.83 95.31 L 307.83 91.66 L 311.5 86.66 L 314.18 88 L 315.83 93.31 Z M 321.66 101.34 L 315.66 97.66 L 318.34 92.34 L 321.02 93.68 L 323.66 99.34 Z M 330 105 L 324 101 L 326 96 L 330 95 L 334 96 L 336 101 Z M 338.34 101.34 L 332.34 97.66 L 341.68 93.68 L 339.02 92.34 L 344.34 99.34 Z M 346.17 95.31 L 340.17 91.66 L 348.5 86.66 L 345.82 88 L 348.17 93.31 Z M 354.12 88.66 L 348.46 84.32 L 359.81 79.88 L 356.47 80.5 L 357.16 86.66 Z M 361.34 82.5 L 355.68 77.5 L 365.31 73.83 L 362.63 75.17 L 364.34 80.5 Z M 370 80 L 368 75 L 372 70 L 375 70 L 377 75 L 373 80 Z M 361.34 57.5 L 355.68 62.5 L 365.31 66.17 L 362.63 64.83 L 364.34 59.5 Z M 354.12 51.34 L 348.46 55.68 L 359.81 60.12 L 356.47 59.5 L 357.16 53.34 Z M 346.17 44.69 L 340.17 48.34 L 348.5 53.34 L 345.82 52 L 348.17 46.69 Z M 338.34 38.66 L 332.34 42.34 L 341.68 46.32 L 339.02 47.66 L 344.34 40.66 Z M 330 35 L 324 39 L 326 44 L 330 45 L 334 44 L 336 39 Z M 321.66 38.66 L 315.66 42.34 L 318.34 47.66 L 321.02 46.32 L 323.66 40.66 Z M 313.83 44.69 L 307.83 48.34 L 311.5 53.34 L 314.18 52 L 315.83 46.69 Z M 305.88 51.34 L 300.22 55.68 L 305.19 60.12 L 308.53 59.5 L 308.84 53.34 Z M 298.66 57.5 L 294.66 62.5 L 300.31 66.17 L 302.99 64.83 L 301.34 59.5 Z" stroke="black" strokeWidth="0.5" fill="none" />
            <circle cx="330" cy="80" r="10" stroke="black" fill="white" />
         </g>
         
         {/* Small gear connected to hello input */}
-        <g id="input-gear" transform-origin="255 60" className={isSubmitting ? 'animate-spin-fast-reverse' : ''}>
+        <g id="input-gear" style={{transformOrigin: "255px 60px"}} className={isSubmitting ? 'animate-spin-fast-reverse' : ''}>
           <path d="M 255 60 L 253 57 L 255 54 L 257 54 L 259 57 L 257 60 Z M 259.8 61.5 L 257.8 58.5 L 261.2 56.4 L 262.6 57.1 L 261.6 60.3 Z M 263.4 64.8 L 260.6 62.4 L 263.6 59.4 L 265.4 59.8 L 265.2 63.4 Z M 265.2 55.2 L 260.6 57.6 L 263.6 60.6 L 265.4 60.2 L 263.4 56.6 Z M 259.8 58.5 L 257.8 61.5 L 254.4 59.4 L 255.8 58.7 L 254.8 55.5 Z M 250.2 58.5 L 252.2 61.5 L 248.8 63.6 L 247.4 62.9 L 248.4 59.7 Z M 255 70 a 5 5 0 1 0 0 1" stroke="black" strokeWidth="0.5" fill="none" />
         </g>
 
         {/* Small gear connected to big gear */}
-        <g id="middle-gear" transform-origin="275 100" className={isSubmitting ? 'animate-spin-fast' : ''}>
+        <g id="middle-gear" style={{transformOrigin: "275px 100px"}} className={isSubmitting ? 'animate-spin-fast' : ''}>
             <circle cx="275" cy="100" r="10" fill="none" stroke="black" strokeWidth="0.5"/>
             <path d="M 275 90 L 272 88 L 275 86 L 278 88 Z M 283.66 95 L 280.16 92.5 L 285 90 L 285 95 Z M 283.66 105 L 280.16 107.5 L 285 110 L 285 105 Z M 275 110 L 272 112 L 275 114 L 278 112 Z M 266.34 105 L 269.84 107.5 L 265 110 L 265 105 Z M 266.34 95 L 269.84 92.5 L 265 90 L 265 95 Z " stroke="black" fill="none" strokeWidth="0.5"/>
             <circle cx="275" cy="100" r="2" fill="black" />
